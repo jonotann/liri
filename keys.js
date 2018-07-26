bb1ef18c5701d25970f4f1ad2b75ca21f72129ac
jonotann <jonathnrldn@gmail.com>
2018-07-26 17:52:19 -0400

    keys update

--- keys.js
+++ keys.js
@@ -1,14 +1,13 @@
-js
 console.log('this is loaded');
 
 exports.twitter = {
-    consumer_key: process.env.NRsBRJ74cxDe764xlwBzgoE7l,
-    consumer_secret: process.env.GQAam05X4SVBQEBbxIWSxVSpA9rNcDquJMU7VovjRgE8xyux2f,
-    access_token_key: process.env,2606957560:JHrQ4Fdec0y7oSvJcUxnkulV9IvKrfHlWXhVYcJ,
-    access_token_secret: process.env,3:RjlcqBJ4eiAqcAR7KuV0JGFIhWn1pYXfmCvC6kCXrUP5
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
